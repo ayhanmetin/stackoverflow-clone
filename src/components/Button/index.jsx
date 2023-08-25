9e71bbe5ad5101b48fab719f7db02720c39fbc3a
Ayhan Metin <ayhanmet1@gmail.com>
2023-08-25 23:07:54 +0200

    add buttons

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+// "@/components/Button.button.css";
+// import "./button.css"; 
+import "./button.css"
+import React from "react";
+
+const Button = (
+  props
+) => {
+
+
+  return <button {...props} />;
+};
+
+
+export default Button;
+/**
+ * git add .
+ * git commit -m " 
+ */
